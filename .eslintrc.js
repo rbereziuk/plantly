903f43242d98a1a70b87e0bdd927ac9d21fd0039
Roman Bereziuk <roma.physicist@gmail.com>
2024-11-27 23:14:01 +0200

    Set up eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier', 'react-native'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-native/no-unused-styles': 'error',
+  },
+  ignorePatterns: ['/dist/*'],
+};
