b5c6e3a070a1a0dec5aa307fb37b5d9977f8403e
Roman Bereziuk <roma.physicist@gmail.com>
2024-12-02 22:12:55 +0200

    Add a linear gradient background

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,7 +1,9 @@
 import { View, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
 import { theme } from '@/theme';
 import { useUserStore } from '@/store/userStore';
-import { useRouter } from 'expo-router';
 import { Button } from '@/components/Button';
 
 export default function OnboardingScreen() {
@@ -14,9 +16,15 @@ export default function OnboardingScreen() {
   };
 
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={[theme.colorGreen, theme.colorAppleGreen, theme.colorLimeGreen]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.container}
+    >
+      <StatusBar style="light" />
       <Button title="Let me in!" onPress={handlePress} />
-    </View>
+    </LinearGradient>
   );
 }
 
--- theme.ts
+++ theme.ts
@@ -2,4 +2,6 @@ export const theme = {
   colorWhite: '#fff',
   colorGreen: '#29b365',
   colorLeafyGreen: '#206a42',
+  colorAppleGreen: '#a0d36c',
+  colorLimeGreen: '#d0e57e',
 };
