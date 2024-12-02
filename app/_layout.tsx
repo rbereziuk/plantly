0be74bd7718d45973ff27571256f0d0e0fd86096
Roman Bereziuk <roma.physicist@gmail.com>
2024-12-02 11:22:38 +0200

    Set up bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,34 @@
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+import { theme } from '../theme';
 
 export default function AppLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: theme.colorGreen,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
