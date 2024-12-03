8c5cd49707eb4d2dffa32df758d366d67ecd0dac
Roman Bereziuk <roma.physicist@gmail.com>
2024-12-03 17:44:57 +0200

    Add content to new plant modal

--- app/new.tsx
+++ app/new.tsx
@@ -1,11 +1,69 @@
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  TextInput,
+  Alert,
+  ScrollView,
+  View,
+} from 'react-native';
 import { theme } from '@/theme';
+import { Button } from '@/components/Button';
+import { useState } from 'react';
+import { PlantImage } from '@/components/PlantImage';
 
 export default function NewScreen() {
+  const [name, setName] = useState<string>();
+  const [days, setDays] = useState<string>();
+
+  const handleSubmit = () => {
+    if (!name) {
+      return Alert.alert('Validation Error', 'Give your plant a name');
+    }
+
+    if (!days) {
+      return Alert.alert(
+        'Validation Error',
+        `How often does ${name} need to be watered?`,
+      );
+    }
+
+    if (Number.isNaN(Number(days))) {
+      return Alert.alert(
+        'Validation Error',
+        'Watering frequency must be a be a number',
+      );
+    }
+
+    console.log('Adding plant', name, days);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>New plant</Text>
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      keyboardShouldPersistTaps="handled"
+    >
+      <View style={styles.centered}>
+        <PlantImage />
+      </View>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        style={styles.input}
+        placeholder="E.g. Casper the Cactus"
+        autoCapitalize="words"
+      />
+      <Text style={styles.label}>Watering Frequency (every x days)</Text>
+      <TextInput
+        value={days}
+        onChangeText={setDays}
+        style={styles.input}
+        placeholder="E.g. 6"
+        keyboardType="number-pad"
+      />
+      <Button title="Add plant" onPress={handleSubmit} />
+    </ScrollView>
   );
 }
 
@@ -13,7 +71,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: 'center',
+  },
+  contentContainer: {
+    paddingTop: 24,
+    paddingHorizontal: 24,
+    paddingBottom: 100,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: theme.colorLightGrey,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 24,
+    fontSize: 18,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  centered: {
     alignItems: 'center',
   },
 });
