bf7f09583b1a2c306ffc2e78046da3a1aa96619f
Roman Bereziuk <roma.physicist@gmail.com>
2024-12-03 17:27:51 +0200

    Header button to add new plant with empty modal

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
-import { Redirect, Tabs } from 'expo-router';
+import { Pressable } from 'react-native';
+import { Redirect, Tabs, Link } from 'expo-router';
 import Entypo from '@expo/vector-icons/Entypo';
 import Feather from '@expo/vector-icons/Feather';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { theme } from '@/theme';
 import { useUserStore } from '@/store/userStore';
 
@@ -26,6 +28,17 @@ export default function AppLayout() {
           title: 'Home',
           tabBarIcon: ({ size, color }) => (
             <Entypo name="leaf" size={size} color={color} />
+          ),
+          headerRight: () => (
+            <Link href="/new" asChild>
+              <Pressable hitSlop={20} style={{ marginRight: 12 }}>
+                <AntDesign
+                  name="pluscircleo"
+                  size={24}
+                  color={theme.colorGreen}
+                />
+              </Pressable>
+            </Link>
           ),
         }}
       />
